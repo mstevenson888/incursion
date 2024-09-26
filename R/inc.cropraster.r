4590f69b0f1631da02c7a567aeaec166d0724833
mstevenson888 <mstevenson888@gmail.com>
2024-09-26 21:54:21 +1000

    0.2-15

--- R/inc.cropraster.r
+++ R/inc.cropraster.r
@@ -1,16 +1,32 @@
-inc.cropraster <- function (raster, sf, snap = "out", type = "raster"){
+inc.cropraster <- function(x, sf, type = "raster"){
   
-  # Crop the raster to the sf extent:
-  raster.cr <- terra::crop(x = raster, y = raster::extent(sf))
-  
-  # Mask the raster to the polygon boundaries:
-  raster.r <- terra::mask(raster.cr, sf)
-  
-  if (type == "raster") {
-    rval <- raster.r
+  if(is(x, "SpatRaster")){
+    # Crop the terra::spatRaster object to the sf extent:
+    x.cr <- terra::crop(x = x, y = terra::ext(sf))
+    
+    # Mask the terra::spatRaster object to the sf polygon boundaries:
+    x.r <- terra::mask(x.cr, sf)
   }
-  if (type == "data.frame") {
-    rval <- data.frame(raster::rasterToPoints(raster.r))
+  
+  if(is(x, "RasterLayer")){
+    # Crop the raster::RasterLayer to the sf extent:
+    x.cr <- raster::crop(x = x, y = raster::extent(sf))
+    
+    # Mask the raster::RasterLayer to the sf polygon boundaries:
+    x.r <- raster::mask(x.cr, sf)
   }
+
+  if(type == "raster"){
+    rval <- x.r
+  }
+  
+  if(type == "data.frame" & is(x, "SpatRaster")){
+    rval <- terra::as.data.frame(x = x.r, xy = TRUE) 
+  }
+  
+  if(type == "data.frame" & is(x, "RasterLayer")){
+    rval <- raster::rasterToPoints(x = x.r) 
+  }
+  
   return(rval)
 }
